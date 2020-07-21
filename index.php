7d5ef63322a628a72887f5ef7a2a535a0e497ac1
MullerChatBot <68578287+MullerChatBot@users.noreply.github.com>
2020-07-20 23:43:50 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-echo 'Algo';
+<? php
+requiere  '../vendor/autoload.php' ;
+
+$ conexion = new  MongoDB \ Client ( 'mongodb + srv: // Rodrigo: Welcome12345678@cluster0-5grz7.azure.mongodb.net/chatbot? retryWrites = true' );
+$ base = $ conexion -> chatbot ;
+$ coleccion = $ base -> contactos ;
+
+$ busqueda = $ coleccion -> find ();
+foreach ( $ busqueda  como  $ doc ) {
+    echo  json_encode ( $ doc );
+}
